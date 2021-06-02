b0af046d5ec8a18e82693cc1a660fac180991584
Troy Flavell <hunter007nz@gmail.com>
2021-06-02 18:26:21 +1200

    Added update feature

--- frontend/pages/posts/[id].js
+++ frontend/pages/posts/[id].js
@@ -6,47 +6,117 @@ import { useRouter } from "next/router";
 import { API_URL } from "../../utils/urls";
 
 const SinglePost = () => {
-  const [post, setPost] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState({});
-  let color = useState("#000000");
 
-  //router related properties
+    const [post, setPost] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState({});
+    const [edit, setEdit] = useState(false);
+    const [description, setDescription] = useState("")
 
-  const router = useRouter();
-  const { id } = router.query;
 
-  useEffect(() => {
+    //router related properties
+
+    const router = useRouter();
+    const { id } = router.query;
+
+
+    //handlers
+
     const fetchPost = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch(`${API_URL}/posts/${id}`);
-        const data = await response.json();
+        try {
+            setLoading(true);
+            const response = await fetch(`${API_URL}/posts/${id}`);
+            const data = await response.json();
 
-        setPost(data);
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        setError(error);
-      }
+            setPost(data);
+            setDescription(data.description)
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            setError(error);
+        }
     };
-    fetchPost();
-  }, []);
 
-  return (
-    <>
-      <ClipLoader loading={loading} />
+    const editHandler = async (event) => {
+        event.preventDefault()
+        try {
+            setLoading(true)
+            const response = await fetch(`${API_URL}/posts/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                }, 
+                body: JSON.stringify({ 
+                    description
+                })
+            })
+            const data = await response.json()
+            console.log("data", data)
+            setDescription(data)
+            setLoading(false)
+        } catch (error) {
+            setLoading(false)
+            setError(error)
+        }
+        fetchPost();
+        setEdit(false)
+    }
 
-      {!loading && <>{post.id && <Post posts={post} />}</>}
-      {!post.id && (
-        <h2>
-          Get the fuck out of here boi
-          <br />
-            {error && <p>404 post not found</p>}
-        </h2>
-      )}
-    </>
-  );
+    const deleteHandler = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(`${API_URL}/posts/${id}`, {
+                "method": "DELETE"
+            })
+            const data = await response.json()
+            router.back()
+
+        } catch (error) {
+            setLoading(false)
+        }
+    }
+
+    // intial side effect
+
+    useEffect(() => {
+        fetchPost();
+    }, []);
+
+    return (
+        <>
+            <ClipLoader loading={loading} />
+
+            {!loading &&
+                <>
+                    {post.id &&
+                        <>
+                            <Post posts={post} />
+                            <button onClick={deleteHandler} >Delete</button>
+                            <button onClick={() => setEdit(!edit)}>Edit</button>
+                            {edit &&
+                                <form onSubmit={editHandler}>
+                                    <input
+                                        type="text"
+                                        value={description}
+                                        placeholder="New description"
+                                        onChange={(event) => setDescription(event.target.value)}
+                                    />
+                                    <button>Confirm change</button>
+                                </form>
+                            }
+                        </>
+                    }
+                </>
+            }
+            {!post.id && (
+                <h2>
+                    Get the fuck out of here boi
+                    <br />
+                    {error && <p>404 post not found</p>}
+                </h2>
+            )}
+        </>
+    );
 };
 
 export default SinglePost;
